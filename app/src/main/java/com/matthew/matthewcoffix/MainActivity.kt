fbf35c8ac7b5855c2855eb3b79df18821b8abc56
erbzvdxkbnf@gmail.com <erbzvdxkbnf@gmail.com>
2024-10-15 22:19:11 +0300

    We created a project and added some dependencies

---
+++ app/src/main/java/com/matthew/matthewcoffix/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.matthew.matthewcoffix
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.matthew.matthewcoffix.presantation.theme.MatthewCoffixTheme
+import com.matthew.matthewcoffix.ui.OnboardingScreen
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MatthewCoffixTheme {
+                ScaffoldBody()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Composable
+    fun ScaffoldBody(
+
+    ) {
+        // Установка цвета статусбара
+        val navController = rememberAnimatedNavController()
+
+        Scaffold { innerPadding ->
+            AnimatedNavHost(
+                navController = navController,
+                startDestination = "Onboarding",
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable(
+                    "Onboarding",
+                    enterTransition = { fadeIn() },
+                    exitTransition = { fadeOut() }
+                ) {
+                    OnboardingScreen(navController = navController)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/matthew/matthewcoffix/presantation/common/component/DotSlider.kt
@@ -0,0 +1,41 @@
+package com.matthew.matthewcoffix.presantation.common.component
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.matthew.matthewcoffix.presantation.common.viewmodels.IndicatorDot
+
+@Composable
+fun DotSlider(
+    modifier: Modifier,
+    totalDots: Int,
+    selectedIndex: Int,
+    selectedColor: Color,
+    unselectedColor: Color,
+    dotSize: Dp
+) {
+    LazyRow(
+        modifier = Modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+            .padding(12.dp)
+    ) {
+        items(totalDots) { index: Int ->
+            IndicatorDot(
+                modifier = Modifier,
+                size = dotSize,
+                color = if (index == selectedIndex) selectedColor else unselectedColor
+            )
+            if (index != totalDots - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+            }
+        }
+    }
+}
